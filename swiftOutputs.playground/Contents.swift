0ef1c4cfe16fd4e7a31e60e094cb89e2aae7951b
dhywty <clemwek@gmail.com>
2018-02-16 14:11:26 +0300

    Error Handling

--- swiftOutputs.playground/Contents.swift
+++ swiftOutputs.playground/Contents.swift
@@ -19,3 +19,23 @@ extension String {
 
 let name: String = "clement wekesa wafula"
 name.getWords()
+
+//: ### Create a Swift Playground project that handles errors generated from bad inputs into a function and performs a different operation to cater for the error thrown that allows for normal operation of the application.
+
+func randomLuchCheck(_ userInput: String) throws -> String {
+    guard let userInput = Int(userInput) else {
+        return "In put must be a integert"
+    }
+    let randomNumber = Int(arc4random_uniform(10))
+    if randomNumber == userInput {
+        return "Lucky answer, you WIN"
+    }
+
+    return "Try again..."
+}
+
+do {
+  try  randomLuchCheck("5")
+} catch let error {
+print("some error \(error)")
+}
